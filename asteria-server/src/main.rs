ab3abcd3acd65953fe0b010011a4a2516cc7ed61
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-07-06 10:07:45 +0800

    feat(main): replace main function with async version and initialize logging

--- asteria-server/src/main.rs
+++ asteria-server/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Ok, Result};
+use asteria_core::init_logging;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    init_logging();
+
+    Ok(())
 }
